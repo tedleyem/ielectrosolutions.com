518eb6bdfeaa0dec8c9a543c502c4833885557d2
tmeralus <tmeralus@gmail.com>
2022-09-16 17:13:34 -0400

    removed Navlink from headers

--- src/ie-components/hero/TwoColumnWithPrimaryBackground.js
+++ src/ie-components/hero/TwoColumnWithPrimaryBackground.js
@@ -15,8 +15,7 @@ import logoImageSrc from "images/ielogowhite.svg";
 import serverSecureIllustrationImageSrc from "images/server-redundancy-illustration-blue2.svg"
 
 const PrimaryBackgroundContainer = tw.div`-mx-8 px-8 bg-primary-900 text-gray-100`;
-const Header = tw(HeaderBase)`max-w-none -mt-8 py-8 -mx-8 px-8`;
-const NavLink = tw(NavLinkBase)`lg:text-gray-100 lg:hocus:text-gray-300 lg:hocus:border-gray-100`;
+const Header = tw(HeaderBase)`max-w-none -mt-8 py-8 -mx-8 px-8`; 
 const LogoLink = tw(LogoLinkBase)`text-gray-100 hocus:text-gray-300`;
 const PrimaryLink = tw(PrimaryLinkBase)`shadow-raised lg:bg-primary-400 lg:hocus:bg-primary-500`;
 
